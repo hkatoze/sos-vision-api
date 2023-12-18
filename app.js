cfcb086d2904e6a0089fab677955426c5e641f79
hkatoze <harounakinda.pro@gmail.com>
2023-12-18 12:47:01 +0000

    cors origin to *

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 app
-  .use(cors())
+  .use(cors({ origin: "*" }))
   .use(bodyParser.json())
   .use(favicon(__dirname + "/favicon.ico"));
 initDb();
